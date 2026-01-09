22b5784c60772c9b62fbb16534249d86da88a35f
Dan Glorioso <125602370+danglorioso@users.noreply.github.com>
2026-01-08 20:23:40 -0500

    Change hover colors

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,12 +3,12 @@ import Link from "next/link";
 
 export default function Logo() {
     return (
-        <Link href="/" className="flex items-center hover:bg-gray-100 transition p-2 rounded-lg">
+        <Link href="/" className="flex items-center group transition p-2 rounded-lg">
             <div className="flex items-center gap-2">
                 {/* Logo */}
                 <Trees className="w-8 h-8 text-green-600" />
                 {/* Title */}
-                <span className="text-xl font-bold text-gray-900">ParkQuest</span>
+                <span className="text-xl font-bold text-gray-900 group-hover:text-green-600 duration-200">ParkQuest</span>
             </div>
         </Link>
     );
